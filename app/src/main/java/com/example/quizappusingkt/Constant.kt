1e3ccd2e7a39738b3b53ef9a9e179bb9ceab8a83
merajuni7ed <merajmusavi1243m@gmail.com>
2023-01-28 00:15:00 +0330

    update

--- app/src/main/java/com/example/quizappusingkt/Constant.kt
+++ app/src/main/java/com/example/quizappusingkt/Constant.kt
@@ -7,9 +7,9 @@ class Constant {
         fun createObj():List<DataModel>{
             var questions = ArrayList<DataModel>()
             val q1 = DataModel("which country is?",1,R.drawable.ic_flag_of_argentina,"iran","argentina","usa","brazil",2)
-            val q2 = DataModel("which country is?",1,R.drawable.ic_flag_of_belgium,"iran","argentina","usa","belgium",2)
-            val q3 = DataModel("which country is?",1,R.drawable.ic_flag_of_australia,"iran","argentina","australia","brazil",2)
-            val q4 = DataModel("which country is?",1,R.drawable.ic_flag_of_germany,"germany","argentina","usa","brazil",2)
+            val q2 = DataModel("which country is?",1,R.drawable.ic_flag_of_belgium,"iran","argentina","usa","belgium",4)
+            val q3 = DataModel("which country is?",1,R.drawable.ic_flag_of_australia,"iran","argentina","australia","brazil",3)
+            val q4 = DataModel("which country is?",1,R.drawable.ic_flag_of_germany,"germany","argentina","usa","brazil",1)
             questions.add(q1)
             questions.add(q2)
             questions.add(q3)
--- app/src/main/java/com/example/quizappusingkt/QuestionActivity.kt
+++ app/src/main/java/com/example/quizappusingkt/QuestionActivity.kt
@@ -28,8 +28,8 @@ class QuestionActivity : AppCompatActivity() {
     }
 
 
-    fun reset_to_defualt(){
-      var textview_array = ArrayList<TextView>()
+    fun reset_to_defualt() {
+        var textview_array = ArrayList<TextView>()
         textview_array.add(binding.option1)
         textview_array.add(binding.option2)
         textview_array.add(binding.option3)
@@ -37,7 +37,7 @@ class QuestionActivity : AppCompatActivity() {
 
         for (textView in textview_array) {
             textView.typeface = Typeface.DEFAULT
-            textView.background = ContextCompat.getDrawable(this,R.drawable.bg_option)
+            textView.background = ContextCompat.getDrawable(this, R.drawable.bg_option)
         }
     }
 
@@ -51,29 +51,64 @@ class QuestionActivity : AppCompatActivity() {
         binding.tvFlag.setImageResource(question.image)
     }
 
-    fun OnOptionClicked(view:View){
+    fun OnOptionClicked(view: View) {
         reset_to_defualt()
 
         val btn = view as TextView
-        when(btn.tag.toString()){
-            "1"->{which_option=1 }
-            "2"->{which_option=2 }
-            "3"->{which_option=3 }
-            "4"->{which_option=4 }
+        when (btn.tag.toString()) {
+            "1" -> {
+                which_option = 1
+            }
+            "2" -> {
+                which_option = 2
+            }
+            "3" -> {
+                which_option = 3
+            }
+            "4" -> {
+                which_option = 4
+            }
         }
         btn.typeface = Typeface.DEFAULT_BOLD
-        btn.background = ContextCompat.getDrawable(this,R.drawable.bg_option_after_selection)
+        btn.background = ContextCompat.getDrawable(this, R.drawable.bg_option_after_selection)
     }
-    fun onSubmitClicked(view:View){
-        if (which_option==0 && current_q<=2){
+
+    fun onSubmitClicked(view: View) {
+        if (which_option == 0 && current_q <= 2) {
             current_q++
 
             dataset()
 
-        }else{
-             current_q = 0
-            dataset()
+        } else {
+            if (which_option == questions[current_q].correct) {
+                setBgForTv(questions[current_q].correct,R.drawable.bg_option_true)
 
+
+            } else {
+                setBgForTv(which_option, R.drawable.bg_option_false)
+                setBgForTv(questions[current_q].correct,R.drawable.bg_option_true)
+
+            }
+
+        }
+    }
+
+    fun setBgForTv(id: Int, draw: Int) {
+        when (id) {
+            1 -> {
+                binding.option1.background = ContextCompat.getDrawable(this, draw)
+            }
+            2 -> {
+                binding.option2.background = ContextCompat.getDrawable(this, draw)
+            }
+            3 -> {
+                binding.option3.background = ContextCompat.getDrawable(this, draw)
+
+            }
+            4 -> {
+                binding.option4.background = ContextCompat.getDrawable(this, draw)
+
+            }
         }
     }
 }
